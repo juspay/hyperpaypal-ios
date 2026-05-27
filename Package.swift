d053fd26279c459ed75745145a10a3941e542b2a
Balaganesh S <balaganesh@juspay.in>
2026-05-27 16:10:52 +0530

    Updated Package for version 2.2.7.7

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HyperPayPal",
-            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.7.6/HyperPayPal.zip",
-            checksum: "cc148d603293d048be2ee2f8c441b747fcf611150c2f46c832f908fa1bdcbaf2"
+            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.7.7/HyperPayPal.zip",
+            checksum: "4d1c612e5ca04a8c06f45833cb1c80a0cc2f2f779d164b672222c47a30a985f0"
         ),
         .binaryTarget(
             name: "PPRiskMagnes",
